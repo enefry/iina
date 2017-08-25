64dcf2e7fd1aa227de69719b41f5433aa95c8504
Collider LI <lhc199652@gmail.com>
2017-08-25 19:34:47 +0800

    more_filters: finished

--- iina/Extensions.swift
+++ iina/Extensions.swift
@@ -189,7 +189,7 @@ extension NSPoint {
 
 extension Array {
   func at(_ pos: Int) -> Element? {
-    if pos < count {
+    if pos >= 0 && pos < count {
       return self[pos]
     } else {
       return nil
@@ -227,6 +227,12 @@ extension Int {
     if self < min { value = min }
     if self > max { value = max }
     return value
+  }
+}
+
+extension Float {
+  func toStr() -> String {
+    return "\(self)"
   }
 }
 
